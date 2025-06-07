a606f44f1f960a47c79e9a2087f61d77338d493c
Mattias Kronander Ross <fuuijin@gmail.com>
2025-06-07 11:18:27 +0200

    Updated :rocket:

---
+++ src/colors.lua
@@ -0,0 +1,27 @@
+return {
+	foreground = "#CDD6F4",
+	background = "#1E1E2E",
+	cursor_bg = "#F5E0DC",
+	colors = {
+		ansi = {
+			"#45475A",
+			"#F38BA8",
+			"#A6E3A1",
+			"#F9E2AF",
+			"#89B4FA",
+			"#F5C2E7",
+			"#94E2D5",
+			"#BAC2DE",
+		},
+		brights = {
+			"#585B70",
+			"#F38BA8",
+			"#A6E3A1",
+			"#F9E2AF",
+			"#89B4FA",
+			"#F5C2E7",
+			"#94E2D5",
+			"#A6ADC8",
+		},
+	},
+}
